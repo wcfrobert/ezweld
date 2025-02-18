1ebcbf1f9a99d4d73e67ad7ea129e2aac4d11e8c
Robert W <55780110+wcfrobert@users.noreply.github.com>
2025-02-17 22:31:31 -0800

    updated version number

--- ezweld/__init__.py
+++ ezweld/__init__.py
@@ -4,7 +4,7 @@ to both in-plane and out-of-plane forces in all six degrees of freedom via the
 elastic method.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Robert Wang"
 __license__ = "MIT"
 
